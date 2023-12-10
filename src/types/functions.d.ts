aad45e67120e551805c6f304f08fe8e39142cfe5
pengzhanbo <volodymyr@foxmail.com>
2023-12-11 06:03:48 +0800

    feat: add css `function` types

---
+++ src/types/functions.d.ts
@@ -0,0 +1,5 @@
+export type CSSFunction =
+  | 'abs' | 'acos' | 'asin' | 'atan' | 'atan2' | 'attr' | 'calc' | 'clamp'
+  | 'cos' | 'counter' | 'counters' | 'cross-fade' | 'env' | 'exp' | 'fix-content'
+  | 'hypot' | 'log' | 'max' | 'min' | 'minmax' | 'pow' | 'rem' | 'repeat'
+  | 'round' | 'sign' | 'sin' | 'sqrt' | 'symbols' | 'tan' | 'url' | 'var'
