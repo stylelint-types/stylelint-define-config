2f96f1c00835938686c8740d54c18881898073ba
pengzhanbo <volodymyr@foxmail.com>
2023-12-11 06:02:17 +0800

    chore: update vsc settings

--- src/types.d.ts
+++
@@ -1,7 +0,0 @@
-import type * as CSS from 'csstype'
-
-export type CSSPropertiesMap = keyof CSS.Properties
-
-export type StandardPropertiesMap = keyof CSS.StandardProperties
-
-export type AtRules = CSS.AtRules extends `@${infer T}` ? T : never
