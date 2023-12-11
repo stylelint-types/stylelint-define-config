90def8392c01fba7ffa7c3bde1d56427bb778a8b
pengzhanbo <volodymyr@foxmail.com>
2023-12-12 02:25:46 +0800

    docs: update readme

---
+++ example/stylelint.config.js
@@ -0,0 +1,12 @@
+import defineConfig from 'stylelint-define-config'
+
+export default defineConfig({
+  extends: ['stylelint-config-standard'],
+  rules: {
+    'unit-allowed-list': [['px'], {
+      ignoreProperties: {
+        px: ['border'],
+      },
+    }],
+  },
+})
