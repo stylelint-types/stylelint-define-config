f96169e54bbfc185188eeb4918f070a50b8f5ec9
pengzhanbo <volodymyr@foxmail.com>
2023-12-12 02:25:29 +0800

    feat: add entry files

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from './define-config'
+
+export * from './define-config'
+export type * from './types'
+export type * from './rules/rule-config'
+export type * from './rules/stylelint'
+export type * from './config'
+export type * from './utils'
+
+export type { CustomSyntax } from './config/customSyntax'
+export type { CustomPlugins } from './config/plugins'
+export type { CustomRuleOptions } from './rules'
+export type { CustomExtends } from './config/extends'
+
+export default defineConfig
