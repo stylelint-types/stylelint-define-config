3278ed012616f7361bd2400d752be0abdbc0db5b
pengzhanbo <volodymyr@foxmail.com>
2023-12-11 17:29:39 +0800

    chore: update package

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
   outDir: 'dist',
   clean: true,
   target: ['es2022', 'node18'],
