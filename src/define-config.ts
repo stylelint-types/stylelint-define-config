9be8471e63b4f67f0aac64518f8ec2d55160c195
pengzhanbo <volodymyr@foxmail.com>
2023-12-12 02:25:01 +0800

    feat: add `defineConfig`

---
+++ src/define-config.ts
@@ -0,0 +1,11 @@
+import type { StylelintConfig } from './config'
+
+/**
+ * Define a Stylelint config.
+ *
+ * @param config - Stylelint config
+ * @returns Stylelint config
+ */
+export function defineConfig(config: StylelintConfig) {
+  return config
+}
