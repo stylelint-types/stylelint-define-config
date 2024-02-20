775da81bd54a64ee5356287c540d4f8f1df62c0d
pengzhanbo <volodymyr@foxmail.com>
2024-02-20 19:20:39 +0800

    feat: `checkContextFunctionalPseudoClasses: []` to `selector-max-id`

--- src/rules/stylelint/selector-max-id.d.ts
+++ src/rules/stylelint/selector-max-id.d.ts
@@ -3,6 +3,7 @@ import type { RuleConfig } from '../rule-config'
 export type SelectorMaxIdOptions = RuleConfig<
   number,
   {
+    checkContextFunctionalPseudoClasses?: (string | RegExp)[]
     message?: string | ((selector: string, max: number | string) => string)
   }
 >
