c975bf4b188adcf5773041369de8937e0ad14637
pengzhanbo <volodymyr@foxmail.com>
2024-10-29 11:32:21 +0800

    feat: add support for `string` to `ignoreLonghands` option of `declaration-block-no-redundant-longhand-properties`

--- src/rules/stylelint/declaration-block-no-redundant-longhand-properties.d.ts
+++ src/rules/stylelint/declaration-block-no-redundant-longhand-properties.d.ts
@@ -3,7 +3,7 @@ import type { RuleConfig } from '../rule-config'
 export type DeclarationBlockNoRedundantLonghandPropertiesOptions = RuleConfig<
   true,
   {
-    ignoreLonghands?: string[]
+    ignoreLonghands?: string | string[]
     ignoreShorthands?: (string | RegExp)[]
     message?: string | ((props: string) => string)
   }
