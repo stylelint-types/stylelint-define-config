c0ed9fc11d6a20b4ea32f9869dfc115dc0cf60db
pengzhanbo <volodymyr@foxmail.com>
2024-10-29 11:30:39 +0800

    feat: add support for `string | RegExp` to `ignoreValues` option of `value-no-vendor-prefix`

--- src/rules/stylelint/value-no-vendor-prefix.d.ts
+++ src/rules/stylelint/value-no-vendor-prefix.d.ts
@@ -3,7 +3,7 @@ import type { RuleConfig } from '../rule-config'
 export type ValueNoVendorPrefixOptions = RuleConfig<
   true,
   {
-    ignoreValues: (string | RegExp)[]
+    ignoreValues: string | RegExp | (string | RegExp)[]
     message?: string | ((actual: number, expected: string) => string)
   }
 >
