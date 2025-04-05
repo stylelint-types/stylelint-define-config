a73ff8c553f5f6dfe0a7e21024e973398b499539
pengzhanbo <volodymyr@foxmail.com>
2025-04-06 00:10:17 +0800

    feat: add `ignorePreludeOfAtRules: []` to `length-zero-no-unit`

--- src/rules/stylelint/length-zero-no-unit.d.ts
+++ src/rules/stylelint/length-zero-no-unit.d.ts
@@ -5,8 +5,16 @@ import type { RuleConfig } from '../rule-config'
 export type LengthZeroNoUnitOptions = RuleConfig<
   true,
   {
+    /**
+     * Ignore units for zero length in custom properties.
+     */
     ignore?: ('custom-properties')[]
+  
     ignoreFunctions?: (CSSFunction | RegExpLike)[]
+    /**
+     * Ignore units for zero lengths within the preludes of the specified at-rules.
+     */
+    ignorePreludeOfAtRules?: RegExpLike[]
     message?: string | (() => string)
   }
 >
