de71c9a8140e398d518d3d148f5318bf69813e7b
pengzhanbo <volodymyr@foxmail.com>
2025-04-26 02:32:58 +0800

    feat: add `ignore: ["four-into-three-edge-values"]` to `shorthand-property-no-redundant-values`

--- src/rules/stylelint/shorthand-property-no-redundant-values.d.ts
+++ src/rules/stylelint/shorthand-property-no-redundant-values.d.ts
@@ -4,5 +4,9 @@ export type ShorthandPropertyNoRedundantValuesOptions = RuleConfig<
   true,
   {
     message?: string | ((unexpected: string, expected: string) => string)
+    /**
+     * Ignore four-value shorthand declarations that could be shortened to three values when applied to edges.
+     */
+    ignore?: readonly ['four-into-three-edge-values']
   }
 >
