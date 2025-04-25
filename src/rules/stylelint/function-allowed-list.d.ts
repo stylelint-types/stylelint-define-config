a816c4ba3a6debb8bb7bb86b0ef30a150542b8a1
pengzhanbo <volodymyr@foxmail.com>
2025-04-26 02:30:53 +0800

    feat: add `exceptWithoutPropertyFallback: []` to `function-allowed-list`

--- src/rules/stylelint/function-allowed-list.d.ts
+++ src/rules/stylelint/function-allowed-list.d.ts
@@ -6,5 +6,9 @@ export type FunctionAllowedListOptions = RuleConfig<
   CSSFunction | RegExpLike | (CSSFunction | RegExpLike)[],
   {
     message?: string | ((name: string) => string)
+    /**
+     * Disallow the matching functions when they are without a property fallback in the same declaration block.
+     */
+    exceptWithoutPropertyFallback?: CSSFunction | RegExpLike | (CSSFunction | RegExpLike)[]
   }
 >
