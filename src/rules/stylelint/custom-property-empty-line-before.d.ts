0363113cb70e181520d3d99e6eb030f8f707abf0
pengzhanbo <volodymyr@foxmail.com>
2025-06-23 02:24:33 +0800

    feat: add `ignore: ["after-custom-property"]` to `custom-property-empty-line-before`

--- src/rules/stylelint/custom-property-empty-line-before.d.ts
+++ src/rules/stylelint/custom-property-empty-line-before.d.ts
@@ -5,7 +5,7 @@ export type CustomPropertyEmptyLineBeforeOptions = RuleConfig<
   'always' | 'never',
   {
     except?: LiteralUnion<'after-comment' | 'after-custom-property' | 'first-nested'>[]
-    ignore?: LiteralUnion<'after-comment' | 'first-nested' | 'inside-single-line-block'>[]
+    ignore?: LiteralUnion<'after-comment' | 'first-nested' | 'inside-single-line-block' | 'after-custom-property'>[]
     message?: string | (() => string)
   }
 >
