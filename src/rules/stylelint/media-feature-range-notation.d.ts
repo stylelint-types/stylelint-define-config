d07d7f0b7be344254054e015cd31c10e5986ab19
pengzhanbo <volodymyr@foxmail.com>
2025-09-09 14:33:33 +0800

    feat: add `except: ["exact-value"]` to `media-feature-range-notation`

--- src/rules/stylelint/media-feature-range-notation.d.ts
+++ src/rules/stylelint/media-feature-range-notation.d.ts
@@ -3,6 +3,10 @@ import type { RuleConfig } from '../rule-config'
 export type MediaFeatureRangeNotationOptions = RuleConfig<
   'context' | 'prefix',
   {
+    /**
+     * - `'exact-value'`: Reverse the primary option for media features with exact values.
+     */
+    except?: ('exact-value')[]
     message?: string | ((primary: string) => string)
   }
 >
