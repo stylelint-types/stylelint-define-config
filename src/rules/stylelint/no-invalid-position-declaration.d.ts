11523c3e6e24ec7ba1a928ef7a2574df43d9734d
pengzhanbo <volodymyr@foxmail.com>
2025-11-28 17:49:42 +0800

    feat: add `ignoreAtRules: []` to `no-invalid-position-declaration`

--- src/rules/stylelint/no-invalid-position-declaration.d.ts
+++ src/rules/stylelint/no-invalid-position-declaration.d.ts
@@ -1,3 +1,9 @@
+import type { RegExpLike } from '../../utils'
 import type { RuleConfig } from '../rule-config'
 
-export type NoInvalidPositionDeclarationOptions = RuleConfig<true>
+export type NoInvalidPositionDeclarationOptions = RuleConfig<true, {
+  /**
+   * Ignore nesting at-rules within specified at-rules.
+   */
+  ignoreAtRules?: RegExpLike[]
+}>
