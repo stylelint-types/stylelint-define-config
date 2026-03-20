264493a39aa90c1aa033f654d1cac04e4b50b01e
pengzhanbo <volodymyr@foxmail.com>
2026-03-20 13:52:46 +0800

    feat: deprecated `*syntax` options from `declaration-property-value-no-unknown`

--- src/rules/stylelint/declaration-property-value-no-unknown.d.ts
+++ src/rules/stylelint/declaration-property-value-no-unknown.d.ts
@@ -11,12 +11,16 @@ export type DeclarationPropertyValueNoUnknownOptions = RuleConfig<
      * Extend or alter the properties syntax dictionary.
      * {@link https://github.com/csstree/csstree/blob/master/docs/definition-syntax.md CSS Value Definition Syntax} is used to define a value's syntax.
      * If a definition starts with `|` it is added to the {@link https://csstree.github.io/docs/syntax/ existing definition value} if any.
+     *
+     * @deprecated We've **deprecated** this option and will remove it in the next major release. Use the shared and more performant [languageOptions](https://github.com/stylelint/stylelint/blob/main/docs/user-guide/configure.md#languageoptions) configuration property instead.
      */
     propertiesSyntax?: Record<KebabCaseStandardPropertiesMap, string>
     /**
      * Extend or alter the types syntax dictionary.
      * {@link https://github.com/csstree/csstree/blob/master/docs/definition-syntax.md CSS Value Definition Syntax} is used to define a value's syntax.
      * If a definition starts with `|` it is added to the {@link https://csstree.github.io/docs/syntax/ existing definition value} if any.
+     *
+     * @deprecated We've **deprecated** this option and will remove it in the next major release. Use the shared and more performant [languageOptions](https://github.com/stylelint/stylelint/blob/main/docs/user-guide/configure.md#languageoptions) configuration property instead.
      */
     typesSyntax?: Record<string, string>
   }
