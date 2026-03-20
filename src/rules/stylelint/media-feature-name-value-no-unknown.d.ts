f47b95e1bf978916734c1d92c0287ae533c58224
pengzhanbo <volodymyr@foxmail.com>
2026-03-20 13:47:15 +0800

    feat: added `ignoreMediaFeatureNameValues: {}` to `media-feature-name-value-no-unknown`

--- src/rules/stylelint/media-feature-name-value-no-unknown.d.ts
+++ src/rules/stylelint/media-feature-name-value-no-unknown.d.ts
@@ -1,6 +1,10 @@
+import type { RegExpLike } from '../../utils'
 import type { RuleConfig } from '../rule-config'
 
 export type MediaFeatureNameValueNoUnknownOptions = RuleConfig<
   true,
-  [name: string, value: string]
+  [name: string, value: string],
+  {
+    ignoreMediaFeatureNameValues?: RegExpLike[]
+  }
 >
