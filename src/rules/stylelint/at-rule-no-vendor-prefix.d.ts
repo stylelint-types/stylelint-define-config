b108b32aa79fffc49cc9743d51c76b21c32e0443
pengzhanbo <volodymyr@foxmail.com>
2026-03-20 13:40:20 +0800

    feat: added `ignoreAtRules: []` to `at-rule-no-vendor-prefix`

--- src/rules/stylelint/at-rule-no-vendor-prefix.d.ts
+++ src/rules/stylelint/at-rule-no-vendor-prefix.d.ts
@@ -1,6 +1,10 @@
+import type { AtRules } from '../../types'
 import type { RuleConfig } from '../rule-config'
 
 export type AtRuleNoVendorPrefixOptions = RuleConfig<
   true,
-  [atRule: string]
+  [atRule: string],
+  {
+    ignoreAtRules: (AtRules | RegExp)[]
+  }
 >
