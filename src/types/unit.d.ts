1475c999af303efa02bd38c555d00a8c1ba5c433
pengzhanbo <volodymyr@foxmail.com>
2026-05-04 03:19:29 +0800

    chore: improve unit types

--- src/types/unit.d.ts
+++ src/types/unit.d.ts
@@ -1,8 +1,14 @@
 import type { LiteralUnion } from '../utils'
 
-export type RelativeUnit = 'em' | 'rem' | 'vw' | 'vh' | 'vmin' | 'vmax' | 'ex' | 'cap' | 'ch' | 'ic' | 'lh' | 'rlh' | 'vi' | 'vb' | 'svw' | 'svh' | 'lvw' | 'lvh' | 'dvw' | 'dvh'
+export type FontRelativeUnit
+  = | 'cap' | 'ch' | 'em' | 'ex' | 'ic' | 'lh' // local font
+    | 'rcap' | 'rch' | 'rem' | 'rex' | 'ric' | 'rlh' // root font
+
+export type ViewportUnit = 'dvh' | 'dvw' | 'lvh' | 'lvw' | 'svh' | 'svw' | 'vb' | 'vh' | 'vw' | 'vi' | 'vmax' | 'vmin'
 
 export type AbsoluteUnit = 'px' | 'pt' | 'cm' | 'mm' | 'Q' | 'in' | 'pc'
+
+export type ContainerUnit = 'cqb' | 'cqh' | 'cqi' | 'cqmax' | 'cqmin' | 'cqw'
 
 export type AngleUnit = 'deg' | 'grad' | 'rad' | 'turn'
 
@@ -12,8 +18,12 @@ export type TimeUnit = 'ms' | 's'
 
 export type FrequencyUnit = 'Hz' | 'kHz'
 
-export type ResolutionUnit = 'dpi' | 'dpcm' | 'dppx'
+export type FlexUnit = 'fr'
 
-export type CustomUnit = 'rpx'
+export type ResolutionUnit = 'dpi' | 'dpcm' | 'dppx' | 'x'
 
-export type Unit = LiteralUnion<RelativeUnit | AbsoluteUnit | AngleUnit | PercentUnit | TimeUnit | FrequencyUnit | ResolutionUnit | CustomUnit>
+export type CustomUnit = 'rpx' /* 小程序 */
+
+export type StrictUnit = FontRelativeUnit | AbsoluteUnit | ViewportUnit | ContainerUnit | AngleUnit | PercentUnit | TimeUnit | FlexUnit | FrequencyUnit | ResolutionUnit | CustomUnit
+
+export type Unit = LiteralUnion<StrictUnit>
